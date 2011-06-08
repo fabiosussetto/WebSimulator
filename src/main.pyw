e9622e25827a305f4ecbccf074e58d287a1bfd0f
Fabio Sussetto <fabio.sussetto@gmail.com>
2011-06-08 21:21:06 +0200

    first commit

---
+++ src/main.pyw
@@ -0,0 +1,65 @@
+'''
+Created on May 2, 2011
+
+@author: fabio
+'''
+
+import sys
+import time
+import simulation
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtWebKit import *
+
+
+class Form(QDialog):
+    
+    def __init__(self, application, parent=None):
+        super(Form, self).__init__(parent)
+        self.simulator = simulation.Simulator(application)
+        
+        self.urlBar = QLineEdit()
+        self.urlGo = QPushButton("Vai")
+        self.btnSimulate = QPushButton("Run test")
+        self.connect(self.btnSimulate, SIGNAL("clicked()"), self._onSimulateClicked)
+        
+        self.simulator.createView()
+        self.browser = self.simulator.getWidget()
+        self.browser.show()
+        
+        urlLayout = QHBoxLayout()
+        urlLayout.addWidget(self.urlBar)
+        urlLayout.addWidget(self.urlGo)
+        
+        layout = QVBoxLayout()
+        layout.addLayout(urlLayout)
+        layout.addWidget(self.browser)
+        layout.addWidget(self.btnSimulate)
+        self.setLayout(layout)
+        
+        self.setWindowTitle("Test Webkit")
+        self.resize(1100, 650)
+        self.simulator.load('http://www.trenitalia.it')
+        #self.simulator.load('http://www.alistapart.com')
+        
+    def _onSimulateClicked(self):
+        self.simulator.click('#menu1 > li:first > a', True)
+        #self.simulator.click('#menu1 > li:nth-child(2) > a', True)
+        self.simulator.fill('#stazin', 'torino')
+        self.simulator.fill('#stazout', 'milano')
+        self.simulator.click('.btnInviaCrusc', True)
+        #self.simulator.fill('#search', 'test')
+        #self.simulator.fill('#search', 'test2')
+        #self.simulator.runjs('alert("cia");')
+        #self.simulator.runjs('alert("cia2");')
+        #self.simulator.click('#submit', True)
+        
+        
+if __name__ == '__main__':
+    pass
+
+
+app = QApplication(sys.argv)
+form = Form(app) 
+form.show() 
+app.exec_()
---
+++ src/simulation/simulator.py
@@ -0,0 +1,135 @@
+'''
+Created on May 31, 2011
+
+@author: fabio
+'''
+
+import os
+import time
+
+from PyQt4.QtCore import SIGNAL, QUrl, QString, Qt, QEvent
+from PyQt4.QtCore import QSize, QDateTime, QPoint
+from PyQt4.QtGui import QApplication, QImage, QPainter
+from PyQt4.QtGui import QCursor, QMouseEvent, QKeyEvent
+from PyQt4.QtNetwork import QNetworkCookie, QNetworkAccessManager
+from PyQt4.QtNetwork import QNetworkCookieJar, QNetworkRequest, QNetworkProxy
+from PyQt4.QtWebKit import QWebPage, QWebView
+
+
+class Simulator(object):
+    '''
+    classdocs
+    '''
+    _jquery = 'jquery-1.6.1.js'
+    _jquery_simulate = 'jquery.simulate.js'
+
+    def __init__(self, application):
+        '''
+        Constructor
+        '''
+        self.application = application
+        self.webpage = QWebPage()
+        #self.webpage.userAgentForUrl = 'test'
+        self.webframe = self.webpage.mainFrame()
+        
+        self.webpage.connect(self.webpage, SIGNAL('loadFinished(bool)'), self._on_load_finished)
+        self.webpage.connect(self.webpage, SIGNAL("loadStarted()"), self._on_load_started)
+        
+        self.jquery = open(os.path.join(os.path.dirname(__file__), "../javascript/" + self._jquery)).read()
+        self.jquery_simulate = open(os.path.join(os.path.dirname(__file__), "../javascript/" + self._jquery_simulate)).read()
+        
+        self.jslib = '$'
+        self._load_status = None
+        
+    def createView(self):
+        self.webview = QWebView()
+        self.webview.setPage(self.webpage)
+        
+    def getWidget(self):
+        return self.webview
+        
+    def _on_load_started(self):
+        self._load_status = None
+    
+    def _on_load_finished(self, successful):
+        self._load_status = successful
+        self.load_js()
+    
+    
+    def load(self, url):
+        """Load a web page and return status (a boolean)."""
+        self.webframe.load(QUrl(url))
+        return self._wait_load()
+
+    def click(self, selector, wait_load=False, wait_requests=None, timeout=None):
+        jscode = "%s('%s').simulate('click');" % (self.jslib, selector)
+        self.runjs(jscode)
+        return self._wait_load(timeout)
+        #self.wait_requests(wait_requests)
+        
+    def runjs(self, jscode, debug=True):
+        res = self.webframe.evaluateJavaScript(jscode)
+        return res    
+        
+    def _wait_load(self, timeout=None):
+        self.application.processEvents()
+        if self._load_status is not None:
+            load_status = self._load_status
+            self._load_status = None
+            return load_status
+        itime = time.time()
+        while self._load_status is None:
+            if timeout and time.time() - itime > timeout:
+                raise SpynnerTimeout("Timeout reached: %d seconds" % timeout)
+            self.application.processEvents()
+        self.application.processEvents()    
+        if self._load_status:
+            #self.load_js()
+            self.webpage.setViewportSize(self.webpage.mainFrame().contentsSize())
+        load_status = self._load_status
+        self._load_status = None
+        return load_status
+    
+    def load_jquery(self, force=False):
+        """Load jquery in the current frame"""
+        jscode = ''
+        jscode += self.jquery
+        jscode += "\nvar %s = jQuery.noConflict();" % self.jslib
+        self.runjs(jscode)
+
+    def load_js(self):
+        self.load_jquery()
+        self.load_jquery_simulate()
+        #self.load_additional_js()
+        
+    def get_js_obj_length(self, res):
+        if res.type() != res.Map:
+            return False
+        resmap = res.toMap()
+        lenfield = QString(u'length')
+        if lenfield not in resmap:
+            return False
+        return resmap[lenfield].toInt()[0]    
+
+    def load_jquery_simulate(self, force=False):
+        """Load jquery in the current frame"""
+        self.runjs(self.jquery_simulate, debug=False)
+        
+    def fill(self, selector, value):
+        """Fill an input text with a string value using a jQuery selector."""
+        escaped_value = value.replace("'", "\\'")
+        jscode = "%s('%s').val('%s');" % (self.jslib, selector, escaped_value)
+        self.runjs(jscode)
+        self.runjs('$();')
+
+class SpynnerError(Exception):
+    """General Spynner error."""
+
+class SpynnerPageError(Exception):
+    """Error loading page."""
+
+class SpynnerTimeout(Exception):
+    """A timeout (usually on page load) has been reached."""
+    
+class SpynnerJavascriptError(Exception):
+    """Error on the injected Javascript code."""
